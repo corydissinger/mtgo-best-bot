27a5d85693c4c3b7c0b2699159661563c749ed1a
corydissinger <cory.dissinger@gmail.com>
2017-06-06 23:11:51 -0400

    Fixed wiring

--- bot-api/src/main/java/com/cd/bot/api/controller/LifecycleEventController.java
+++ bot-api/src/main/java/com/cd/bot/api/controller/LifecycleEventController.java
@@ -20,26 +20,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class LifecycleEventController {
+
     public static final String ENDPOINT_ROOT = "/event";
 
     @Autowired
     LifecycleEventRepository lifecycleEventRepository;
 
+    @Autowired
+    PlayerBotRepository playerBotRepository;
+
     @RequestMapping(value = ENDPOINT_ROOT + "/screen-test", method = RequestMethod.GET)
     @PreAuthorize(BotApiApplication.HAS_AUTH_ROLE_ORCHESTRATOR)
-    private AssumedScreenTest[] getScreenTestEnums() {
+    public AssumedScreenTest[] getScreenTestEnums() {
         return AssumedScreenTest.values();
     }
 
     @RequestMapping(value = ENDPOINT_ROOT + "/lifecycle-status", method = RequestMethod.GET)
     @PreAuthorize(BotApiApplication.HAS_AUTH_ROLE_ORCHESTRATOR)
-    private ProcessingLifecycleStatus[] getLifecycleStatus() {
+    public ProcessingLifecycleStatus[] getLifecycleStatus() {
         return ProcessingLifecycleStatus.values();
     }
 
     @RequestMapping(value = ENDPOINT_ROOT, method = RequestMethod.GET)
     @PreAuthorize(BotApiApplication.HAS_AUTH_ROLE_ORCHESTRATOR)
-    private List<LifecycleEvent> get() {
+    public List<LifecycleEvent> get() {
         List<LifecycleEvent> lifecycleEvents = new ArrayList<>();
         lifecycleEventRepository.findAll().forEach(lifecycleEvents::add);
         return lifecycleEvents;
@@ -47,13 +51,14 @@ public class LifecycleEventController {
 
     @RequestMapping(value = ENDPOINT_ROOT + "/{id}", method = RequestMethod.GET)
     @PreAuthorize(BotApiApplication.HAS_AUTH_ROLE_ORCHESTRATOR)
-    private LifecycleEvent get(@PathVariable final String id) {
+    public LifecycleEvent get(@PathVariable final String id) {
         return lifecycleEventRepository.findOne(Long.parseLong(id));
     }
 
     @RequestMapping(value = ENDPOINT_ROOT, method = RequestMethod.POST)
     @PreAuthorize(BotApiApplication.HAS_AUTH_ROLE_ORCHESTRATOR)
-    private ResponseEntity<Void> save(@RequestBody final LifecycleEvent newLifecycleEvent) {
+    public ResponseEntity<Void> save(@RequestBody final LifecycleEvent newLifecycleEvent) {
+        newLifecycleEvent.setPlayerBot(playerBotRepository.findByName(newLifecycleEvent.getPlayerBot().getName()));
         lifecycleEventRepository.save(newLifecycleEvent);
 
         return ResponseEntity.ok(null);
